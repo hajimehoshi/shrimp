b5968ea72d90858690e7e484ef67261ba6a84beb
hajimehoshi <hajimehoshi@gmail.com>
2009-12-30 04:47:29 +0900

    Added Shrimp.IView (Redo)

---
+++ Shrimp.IViews/IMainForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Shrimp.IViews
+{
+    public interface IMainForm
+    {
+        event EventHandler NewButtonClicked;
+        event EventHandler OpenButtonClicked;
+        event EventHandler CloseButtonClicked;
+        event EventHandler SaveButtonClicked;
+        event EventHandler UndoButtonClicked;
+        event EventHandler PassageButtonClicked;
+        event EventHandler SelectedTileSetChanged;
+        event FormClosingEventHandler FormClosing;
+    }
+}
