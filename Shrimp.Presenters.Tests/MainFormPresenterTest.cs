ac191299fa84a3a0e8e127be0a8a3f8810ca3581
hajimehoshi <hajimehoshi@gmail.com>
2010-01-01 08:08:43 +0900

    Added Shrimp.Presenters

---
+++ Shrimp.Presenters.Tests/MainFormPresenterTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Shrimp.Presenters;
+
+namespace Shrimp.Presenters.Tests
+{
+    [TestFixture]
+    public class MainFormPresenterTest
+    {
+        [Test]
+        public void FooTest()
+        {
+        }
+    }
+}
