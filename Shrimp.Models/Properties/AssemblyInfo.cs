ec757f5744d7e3a8fdb4fa7ad2da8c74d3db65e3
hajimehoshi <hajimehoshi@gmail.com>
2010-01-04 04:04:01 +0900

    Added InternalsVisibleTo attributes

--- Shrimp.Models/Properties/AssemblyInfo.cs
+++ Shrimp.Models/Properties/AssemblyInfo.cs
@@ -35,4 +35,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
-[assembly: InternalsVisibleTo("Shrimp.Tests")]
+#if DEBUG
+[assembly: InternalsVisibleTo("Shrimp.Models.Tests")]
+#endif
