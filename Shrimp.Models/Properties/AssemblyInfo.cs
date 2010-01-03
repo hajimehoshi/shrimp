c8e67642d8d4a02141b77f4fdd08c0c7fc1a1751
hajimehoshi <hajimehoshi@gmail.com>
2010-01-04 04:10:54 +0900

    Added CLSCompliant attributes

--- Shrimp.Models/Properties/AssemblyInfo.cs
+++ Shrimp.Models/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -34,6 +35,8 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: CLSCompliant(true)]
 
 #if DEBUG
 [assembly: InternalsVisibleTo("Shrimp.Models.Tests")]
